76ebf653340e06476c47263ea1bfaa0800473d66
Ilya Petrov <ilya.petrov.m@gmail.com>
2016-08-15 18:26:40 +0300

    Удаление объектов с карты

--- map.js
+++ map.js
@@ -15,18 +15,18 @@ var mapEditing = false;
 var mouseIsDown = false;
 map.addEventListener("mousemove", function(ev) {
 	if(mouseIsDown&&!mapEditing){
-	    map.style.left=ev.clientX+startX+"px";
+	    map.style.left=ev.clientX+startX+"px"; // Перемещение карты
 	    map.style.top=ev.clientY+startY+"px";
 	}
 	if(mouseIsDown&&mapEditing){
-		rect.style.height = Math.abs(parseInt(rect.style.top)-parseInt(ev.clientY))+"px";
+		rect.style.height = Math.abs(parseInt(rect.style.top)-parseInt(ev.clientY))+"px"; // Редактирование
 		rect.style.width = Math.abs(parseInt(rect.style.left)-parseInt(ev.clientX))+"px";
 	}
 }, false);
     map.addEventListener("mousedown", function(ev) {
 	mouseIsDown=true;
 	if(!mapEditing){
-	    startX=-ev.clientX+parseInt(map.style.left);
+	    startX=-ev.clientX+parseInt(map.style.left); // Перемещение карты
 	    startY=-ev.clientY+parseInt(map.style.top);
 	}else{
 		if(mapEditing&&passwCorrect){
@@ -42,7 +42,7 @@ map.addEventListener("mousemove", function(ev) {
 	map.addEventListener("mouseup", function(ev) {
 	    mouseIsDown=false;
 	    drawMap();
-	    if(mapEditing&&passwCorrect){
+	    if(mapEditing&&passwCorrect){ // редактирование
 	    	objForm.style.display="block";
 			objForm.style.top = rect.style.top;
 			objForm.style.left = rect.style.left;
@@ -54,21 +54,22 @@ map.addEventListener("mousemove", function(ev) {
 			objTitle.style.width = rect.style.width;
 
 			objSubmit.onclick = function (){
-				var a = document.createElement('a');
+				var div = document.createElement('div');
+
+				var a = document.createElement('a'); // Добавление элемента
 				a.href = objHref.value;
 				a.innerText = objTitle.value;
 				a.target="_top";
 				
-				a.style.top = parseInt(rect.style.top)-parseInt(map.style.top)+"px";
-				a.style.left = parseInt(rect.style.left)-parseInt(map.style.left)+"px";
-				a.style.height =rect.style.height;
-				a.style.width = rect.style.width;
-				a.onhover = "editLink(this)";
-				
+				div.style.top = parseInt(rect.style.top)-parseInt(map.style.top)+"px";
+				div.style.left = parseInt(rect.style.left)-parseInt(map.style.left)+"px";
+				div.style.height =rect.style.height;
+				div.style.width = rect.style.width;
 
-				mapObj.appendChild(a);
-				console.log(a);
-				addObject(a.outerHTML);
+				div.appendChild(a);
+				mapObj.appendChild(div);
+
+				addObject(div.outerHTML); // Отправка данных на сервер
 	
 				objForm.style.display="none";
 				rect.style.display ="none";
@@ -78,11 +79,9 @@ map.addEventListener("mousemove", function(ev) {
 			}
 		}
 	  }, false);
-function editLink(a){
-	a.style.border = "orange";
-}
 
-function initMap() {
+
+function initMap() { // Первоначальная отрисовка карты
 	for(var i=0;i<pictInRow*pictInCol;i++){
 				var div = document.createElement('div');
 			    div.style.background = "url('default.jpg')";
@@ -96,6 +95,7 @@ function initMap() {
 		map.appendChild(nav);	
 }
 
+// Отрисовка карты
 function drawMap(){
 	var leftBorder = parseInt(Math.abs(parseInt(map.style.left))/(pictW*parseInt(map.style.zoom)*0.01)); 
 	var rightBorder = parseInt((document.documentElement.clientWidth + Math.abs(parseInt(map.style.left)))/(pictW*parseInt(map.style.zoom)*0.01));
@@ -105,9 +105,10 @@ function drawMap(){
 		if((i-1)%pictInRow>=leftBorder && (i-1)%pictInRow<=rightBorder){
 			    map.children[i-1].style.background = "url('map_part_"+i+".jpg')";	  
 		}
-	}
+	}	
 }
 
+// Редактирование
 function toggleEdit(){
 	if(mapEditing){
 		mapEditing=false; 
@@ -116,20 +117,18 @@ function toggleEdit(){
 		objTitle.value = "";
 		objHref.value = "";
 		rect.style.display="none";
-		mapObj.style.pointerEvents = "auto";
 
 		passwForm.style.display="none";
 		passwText.value = "";
 	}
 	else {
 		mapEditing=true;
-		mapObj.style.pointerEvents = "none";
 		if(!passwCorrect)passwForm.style.display="block";
 		else editButton.style.color="green";
 	}
 }
 
-
+// Отправка данных на сервер
 if (window.XMLHttpRequest) request = new XMLHttpRequest(); 
 else if (window.ActiveXObject) {
 try {
@@ -153,7 +152,7 @@ if (request.readyState == 4 && request.status == 200)
 		passw = passwText.value;
 		passwForm.style.display="none";
 		editButton.style.color="green";
-	} else if(request.responseText == "Incorrect password"){
+	} else if(request.responseText == "Incorrect password"){ // Защита паролем
 		passwText.value = "";
 		passwText.style.background="red";
 		passwText.style.color="white";
@@ -164,21 +163,47 @@ if (request.readyState == 4 && request.status == 200)
 			passwText.placeholder = "Введите пароль";
 		}	
 	}
+}else if(request.status == 500){
+	alert("Ошибка на сервере");
 }
 };
 } else alert("Браузер не поддерживает AJAX / AJAX is not supported");
 
 
-passwSubmit.onclick = function(){
+passwSubmit.onclick = function(){ // Защита паролем
 	request.open("POST", 'map_server.php', true);
 	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	// var text = "<a href='"+href+"' style=' top:"+top+"; left:"+left+";'>"+title+"</a>"
 	request.send("passw="+passwText.value+"&type=checkPassw"+"&ajax=1");
 }
 
-function addObject(text){
+function addObject(text){ // Отправка ссылок на сервер
 	request.open("POST", 'map_server.php', true);
 	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	// var text = "<a href='"+href+"' style=' top:"+top+"; left:"+left+";'>"+title+"</a>"
 	request.send("passw="+passw+"&type=add&text="+text+"&ajax=1");
 }
+
+
+// удаление элемента
+function editObj(a){
+	console.log(a);
+	if(mapEditing){
+		for (var i = 0; i < mapObj.children.length; i++) {
+			if(mapObj.children[i] == a) {
+				objForm.style.display="none";
+				mapObj.removeChild(a);
+				request.open("POST", 'map_server.php', true);
+				request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				request.send("passw="+passw+"&type=delete&line_num="+i+"&ajax=1");
+			}
+		}
+	}
+}
+
+function show() {
+  var obj = link.import.querySelectorAll('div');
+  for (var i = 0; i < obj.length; i++) {
+  	mapObj.appendChild(obj[i]);
+  }
+};
+
